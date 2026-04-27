2b114a7306838f32e2c38b7f3a90c3fb2cd0293c
Alex Tudoran <alex.tudoran99@gmail.com>
2026-04-27 12:34:24 +0300

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/rates": ["./data/**/*"],
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
